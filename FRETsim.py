ed95984f65a8104591d3ee3015434886437ae4e8
Mario Rosasco <m.g.rosasco@gmail.com>
2017-04-05 11:59:31 -0700

    Add files via upload
    Added user-defined parameter options, improved some graph options

--- FRETsim.py
+++ FRETsim.py
@@ -9,6 +9,7 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
 
 class FretPair:
     def __init__(self, dist, p1mobility, p2mobility):
@@ -85,11 +86,14 @@ def runSim(N, avgDist, sigma, p1mob, p2mob, dT, finalT, tauDonor, R_0, verbose=T
         distance between donor and acceptor at which energy transfer occurs with 50% efficiency (in Angstrom)
         
     verbose: boolean
-        if True, will report the time at each cycle of the simulation
+        if True, will report the time at each cycle of the simulation and some final summary information
         
     Returns
     -------
-    (startingDists, remainingDists, emittedDists, quenchedDists): 4 numerical lists
+    E: float
+        The calculated energy transfer efficiency. Note that for this to be meaningful, the simulation must have been run long enough
+        for most of the particles to have undergone energy transfer or emission.
+    startingDists, remainingDists, emittedDists, quenchedDists: 4 numerical lists
         Contain the distances for the starting pairs, the remaining pairs, the emitted pairs, and the quenched pairs, respectively
     '''
     # Initialize N pairs of points with separation following a normal distribution centered around some position
@@ -135,44 +139,152 @@ def runSim(N, avgDist, sigma, p1mob, p2mob, dT, finalT, tauDonor, R_0, verbose=T
     k_D = 1.0/tauDonor
     F_D = N*(1 - np.exp(-k_D*finalT))
     F_DA = float(n_emitted)
+    E = 1.0-F_DA/F_D
 
-    print "Of", N, "starting pairs", n_final, "remained."
-    print n_emitted, "emitted during the simulation,"
-    print n_transferred, "underwent energy transfer."
-    print "Calculated transfer efficiency is:", 1.0-F_DA/F_D
+    if verbose:
+        print "Of", N, "starting pairs", n_final, "remained."
+        print n_emitted, "emitted during the simulation,"
+        print n_transferred, "underwent energy transfer."
+        print "Calculated transfer efficiency is:", E
     
-    return (startingDists, remainingDists, emittedDists, quenchedDists)
+    return (E, startingDists, remainingDists, emittedDists, quenchedDists)
     
 if __name__ == '__main__':    
-    # Set up the simulation environment
-    N = 100000
-    avgDist = 6 # in Angstrom
-    sigma = 5 # in Angstrom
-    p1mob = 1 # currently interpreted as width of triangular distribution. 
-    p2mob = 0.01 # currently interpreted as width of triangular distribution. 
-
-    dT = 0.1 # in ns
-    finalT = 10.0 # in ns
-    tauDonor = 2.0 # in ns
-
-    R_0 = 10.0 # in Angstrom
-    
     # Run the simulation
-    startingDists, remainingDists, emittedDists, quenchedDists = runSim(N, avgDist, sigma, p1mob, p2mob, dT, finalT, tauDonor, R_0, verbose=True)
+    while(True):
+        print '''
+        Please select from the following options:
+        1 - Run simulation for single average distance (user defined values)
+        2 - Generate FRET curves (multiple simulations, user defined values)
+        3 - Run simulation for single average distance (demonstration values)
+        4 - Quit
+        '''
+        try:
+            selection=int(raw_input('Please choose an option above: '))
+        except ValueError:
+            print "Invalid selection."
+            continue
+            
+        # quit
+        if selection == 4:
+            quit()
+        
+        elif selection in [1,2,3]:
+            # run single sim     
+            if selection == 1:
+                try:
+                    # Set up the simulation environment
+                    N = int(raw_input('Please enter number of FRET pairs: '))
+                    avgDist = float(raw_input('Please enter average distance between FRET pairs (in Angstrom): '))
+                    sigma = float(raw_input('Please enter std deviation of distance between FRET pairs (in Angstrom): '))
+                    p1mob = float(raw_input('Please enter mobility of FRET donor (width of distribution, in Angstrom): '))
+                    p2mob = float(raw_input('Please enter mobility of FRET acceptor (width of distribution, in Angstrom): '))
+
+                    dT = float(raw_input('Please enter time step for simulation (in ns): '))
+                    finalT = float(raw_input('Please enter total length of simulation (in ns): '))
+                    tauDonor = float(raw_input('Please enter the time constant of the FRET donor (in ns): '))
+
+                    R_0 = float(raw_input('Please enter the R_0 of the FRET pair(in Angstrom): '))
+                except ValueError:
+                    print "Error: could not parse input. Please confirm input data type and try again."
+                    quit()
+            
+                # Run the simulation
+                E, startingDists, remainingDists, emittedDists, quenchedDists = runSim(N, avgDist, sigma, p1mob, p2mob, dT, finalT, tauDonor, R_0, verbose=True)
+                
+                # Show distance histograms
+                ax1 = plt.subplot(311)
+                plt.hist(startingDists,100)    
+                plt.ylabel("# At t=0")
+                
+                ax2 = plt.subplot(312, sharex=ax1, sharey=ax1)
+                plt.hist(emittedDists,100)
+                plt.ylabel("# Emitted")
+                
+                ax3 = plt.subplot(313, sharex=ax1, sharey=ax1)
+                plt.hist(quenchedDists,100)
+                plt.ylabel("# Quenched")
+                
+                plt.xlabel("Distances (Angstrom)")
+                plt.show()
+            
+            # run mutiple sims to make FRET curve   
+            elif selection == 2:
+                try:
+                    # Set up the simulation environment
+                    N = int(raw_input('Please enter number of FRET pairs: '))
+                    sigma = float(raw_input('Please enter std deviation of distance between FRET pairs (in Angstrom): '))
+                    p1mob = float(raw_input('Please enter mobility of FRET donor (width of distribution, in Angstrom): '))
+                    p2mob = float(raw_input('Please enter mobility of FRET acceptor (width of distribution, in Angstrom): '))
+
+                    dT = float(raw_input('Please enter time step for simulation (in ns): '))
+                    finalT = float(raw_input('Please enter total length of simulation (in ns): '))
+                    tauDonor = float(raw_input('Please enter the time constant of the FRET donor (in ns): '))
+
+                    R_0 = float(raw_input('Please enter the R_0 of the FRET pair(in Angstrom): '))
+                except ValueError:
+                    print "Error: could not parse input. Please confirm input data type and try again."
+                    quit()
+                    
+                Evals_immobile = []
+                Evals_mobile = []
+                rvals = []
+                
+                for avgDist in range(1,20,1):
+                    print "Running simulation with average distance =", avgDist
+                    rvals.append(avgDist)
+                    p1mob = 5
+                    p2mob = 5
+                    Evals_mobile.append(runSim(N, avgDist, sigma, p1mob, p2mob, dT, finalT, tauDonor, R_0, verbose=False)[0])
+                    p1mob = 0.00001
+                    p2mob = 0.00001
+                    Evals_immobile.append(runSim(N, avgDist, sigma, p1mob, p2mob, dT, finalT, tauDonor, R_0, verbose=False)[0])
+                    
+                plt.plot(rvals, Evals_mobile, label="mobile donor/acceptor")
+                plt.plot(rvals, Evals_immobile, label="immobile donor/acceptor")
+                plt.ylabel("FRET Efficiency")
+                plt.xlabel("Distance (Angstrom)")
+                plt.legend(['mobile donor/acceptor', 'immobile donor/acceptor'])
+                plt.show()
+            
+            # run single sim with predefined, demonstration values
+            elif selection == 3:
+                # Set up the simulation environment
+                N = 100000
+                avgDist = 6 # in Angstrom
+                sigma = 5 # in Angstrom
+                p1mob = 1 # currently interpreted as width of triangular distribution. 
+                p2mob = 0.01 # currently interpreted as width of triangular distribution. 
+
+                dT = 0.1 # in ns
+                finalT = 10.0 # in ns
+                tauDonor = 2.0 # in ns
+
+                R_0 = 10.0 # in Angstrom
+            
+                # Run the simulation
+                E, startingDists, remainingDists, emittedDists, quenchedDists = runSim(N, avgDist, sigma, p1mob, p2mob, dT, finalT, tauDonor, R_0, verbose=True)
+                
+                # Show distance histograms
+                ax1 = plt.subplot(311)
+                plt.hist(startingDists,100)    
+                plt.ylabel("# At t=0")
+                
+                ax2 = plt.subplot(312, sharex=ax1, sharey=ax1)
+                plt.hist(emittedDists,100)
+                plt.ylabel("# Emitted")
+                
+                ax3 = plt.subplot(313, sharex=ax1, sharey=ax1)
+                plt.hist(quenchedDists,100)
+                plt.ylabel("# Quenched")
+                
+                plt.xlabel("Distances (Angstrom)")
+                plt.show()
+            
+        else:
+            print "Invalid selection."
+            continue
     
-    # Show distance histograms
-    ax1 = plt.subplot(311)
-    plt.hist(startingDists,100)    
-    plt.ylabel("# At t=0")
     
-    ax2 = plt.subplot(312, sharex=ax1, sharey=ax1)
-    plt.hist(emittedDists,100)
-    plt.ylabel("# Emitted")
+        
     
-    ax3 = plt.subplot(313, sharex=ax1, sharey=ax1)
-    plt.hist(quenchedDists,100)
-    plt.ylabel("# Quenched")
-    
-    plt.xlabel("Distances (Angstrom)")
-    plt.locator_params(axis='y', numticks=3) #Why doesn't this work here? Hmm...
-    plt.show()
